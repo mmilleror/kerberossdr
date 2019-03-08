890c305dd2fb3c7f42e4170a452e73343a9b29c3
rtlsdrblog <rtlsdrblog@gmail.com>
2019-03-08 18:15:00 +1300

    CPU Priority changes

--- run.sh
+++ run.sh
@@ -48,8 +48,8 @@ mkfifo _receiver/C/rec_control_fifo
 # Start programs at realtime priority levels
 curr_user=$(whoami)
 #sudo chrt -r 50 ionice -c 1 -n 3 taskset -c 0 ./_receiver/C/rtl_daq $BUFF_SIZE 2>/dev/null 1| sudo chrt -r 50 taskset -c 1 ./_receiver/C/sync $BUFF_SIZE 2>/dev/null 1| sudo chrt -r 50 taskset -c 1 ./_receiver/C/gate $BUFF_SIZE 2>/dev/null 1| sudo nice -n -20 sudo -u $curr_user taskset -c 2,3 python3 -O _GUI/hydra_main_window.py &>/dev/null $BUFF_SIZE $IPADDR&
-sudo nice -n -20 ionice -c 1 -n 3 ./_receiver/C/rtl_daq $BUFF_SIZE 2>/dev/null 1| sudo nice -n -20 ./_receiver/C/sync $BUFF_SIZE 2>/dev/null 1| sudo nice -n -20 ./_receiver/C/gate $BUFF_SIZE 2>/dev/null 1| sudo nice -n -20 sudo -u $curr_user python3 -O _GUI/hydra_main_window.py &>/dev/null $BUFF_SIZE $IPADDR&
-#sudo chrt -r 50 ionice -c 1 -n 3 ./_receiver/C/rtl_daq $BUFF_SIZE 2>/dev/null 1| sudo chrt -r 50 ./_receiver/C/sync $BUFF_SIZE 2>/dev/null 1| sudo chrt -r 50 ./_receiver/C/gate $BUFF_SIZE 2>/dev/null 1| sudo nice -n -20 sudo -u $curr_user python3 -O _GUI/hydra_main_window.py &>/dev/null $BUFF_SIZE $IPADDR&
+#sudo nice -n -20 ionice -c 1 -n 3 ./_receiver/C/rtl_daq $BUFF_SIZE 2>/dev/null 1| sudo nice -n -20 ./_receiver/C/sync $BUFF_SIZE 2>/dev/null 1| sudo nice -n -20 ./_receiver/C/gate $BUFF_SIZE 2>/dev/null 1| sudo nice -n -20 sudo -u $curr_user python3 -O _GUI/hydra_main_window.py &>/dev/null $BUFF_SIZE $IPADDR&
+sudo chrt -r 50 ionice -c 1 -n 3 ./_receiver/C/rtl_daq $BUFF_SIZE 2>/dev/null 1| sudo chrt -r 50 ./_receiver/C/sync $BUFF_SIZE 2>/dev/null 1| sudo chrt -r 50 ./_receiver/C/gate $BUFF_SIZE 2>/dev/null 1| sudo nice -n -20 sudo -u $curr_user python3 -O _GUI/hydra_main_window.py &>/dev/null $BUFF_SIZE $IPADDR&
 
 # Start PHP webserver which serves the updating images
 sudo php -S $IPADDR:8081 -t _webDisplay >&- 2>&-
